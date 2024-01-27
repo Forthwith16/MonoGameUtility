7fdf496342af3a535b63f3e8a318afe256d5e1b0
FireEmblem16 <omacron.storm@gmail.com>
2024-01-27 15:26:31 -0600

    Added yet again more functionality to the IAffineComponent hierarchy.

--- GameEngine/Maths/AffineObject.cs
+++ GameEngine/Maths/AffineObject.cs
@@ -1,4 +1,5 @@
 ﻿using GameEngine.GameComponents;
+using Microsoft.Xna.Framework;
 
 namespace GameEngine.Maths
 {
@@ -52,6 +53,108 @@ namespace GameEngine.Maths
 			Transform = transform;
 			Parent = parent;
 
+			return;
+		}
+
+		public void Translate(float tx, float ty)
+		{
+			Transform = Transform.Translate(tx,ty);
+			return;
+		}
+
+		public void Translate(Vector2 t)
+		{
+			Transform = Transform.Translate(t);
+			return;
+		}
+
+		public void Rotate(float angle, bool righthanded_chirality = false)
+		{
+			Transform = Transform.Rotate(angle,righthanded_chirality);
+			return;
+		}
+
+		public void Rotate(float angle, Vector2 point, bool righthanded_chirality = false)
+		{
+			Transform = Transform.Rotate(angle,point,righthanded_chirality);
+			return;
+		}
+
+		public void Rotate(float angle, float x, float y, bool righthanded_chirality = false)
+		{
+			Transform = Transform.Rotate(angle,x,y,righthanded_chirality);
+			return;
+		}
+
+		public void Rotate90(int times, bool righthanded_chirality = false)
+		{
+			Transform = Transform.Rotate90(times,righthanded_chirality);
+			return;
+		}
+
+		public void Rotate90(int times, Vector2 point, bool righthanded_chirality = false)
+		{
+			Transform = Transform.Rotate90(times,point,righthanded_chirality);
+			return;
+		}
+
+		public void Rotate90(int times, float x, float y, bool righthanded_chirality = false)
+		{
+			Transform = Transform.Rotate90(times,x,y,righthanded_chirality);
+			return;
+		}
+
+		public void Scale(float s)
+		{
+			Transform = Transform.Scale(s);
+			return;
+		}
+
+		public void Scale(float s, float x, float y)
+		{
+			Transform = Transform.Scale(s,x,y);
+			return;
+		}
+
+		public void Scale(float s, Vector2 pos)
+		{
+			Transform = Transform.Scale(s,pos);
+			return;
+		}
+
+		public void Scale(float sx, float sy)
+		{
+			Transform = Transform.Scale(sx,sy);
+			return;
+		}
+
+		public void Scale(float sx, float sy, float x, float y)
+		{
+			Transform = Transform.Scale(sx,sy,x,y);
+			return;
+		}
+
+		public void Scale(float sx, float sy, Vector2 pos)
+		{
+			Transform = Transform.Scale(sx,sy,pos);
+			return;
+		}
+
+		public void Scale(Vector2 s)
+		{
+			Transform = Transform.Scale(s);
+			return;
+		}
+
+		public void Scale(Vector2 s, float x, float y)
+		{
+			Transform = Transform.Scale(s,x,y);
+			return;
+		}
+
+		public void Scale(Vector2 s, Vector2 pos)
+		{
+			Transform = Transform.Scale(s,pos);
 			return;
 		}
 
