59b881ddbd7737f09be20751af39a5bf07190971
Forthwith16 <forthwith.dn@gmail.com>
2025-04-11 02:13:48 -0500

    Created the IGraph interface and its exceptions.

---
+++ GameEngine/DataStructures/Graphs/IGraph.cs
@@ -0,0 +1,300 @@
+﻿using GameEngine.Exceptions;
+
+namespace GameEngine.DataStructures.Graphs
+{
+	/// <summary>
+	/// Represents a graph with vertices and edges connecting vertices.
+	/// </summary>
+	/// <typeparam name="V">The data type stored in vertices.</typeparam>
+	/// <typeparam name="E">The data type stored in edges.</typeparam>
+	public interface IGraph<V,E>
+	{
+		/// <summary>
+		/// Adds a vertex to this graph.
+		/// </summary>
+		/// <param name="data">The data to store in the vertex.</param>
+		/// <returns>Returns the added vertex.</returns>
+		public IVertex<V,E> AddVertex(V data);
+
+		/// <summary>
+		/// Removes a vertex from this graph.
+		/// </summary>
+		/// <param name="v">The vertex to remove.</param>
+		/// <returns>Returns true if <paramref name="v"/> was removed and false otherwise.</returns>
+		/// <exception cref="NoSuchVertexException">Thrown if <paramref name="v"/> does not exist in this graph.</exception>
+		public bool RemoveVertex(IVertex<V,E> v);
+
+		/// <summary>
+		/// Removes a vertex from this graph.
+		/// </summary>
+		/// <param name="v">The vertex to remove.</param>
+		/// <returns>Returns true if <paramref name="v"/> was removed and false otherwise.</returns>
+		public bool TryRemoveVertex(IVertex<V,E> v);
+
+		/// <summary>
+		/// Sets the vertex data of vertex <paramref name="v"/> to <paramref name="data"/>.
+		/// </summary>
+		/// <param name="v">The vertex whose data should be set.</param>
+		/// <param name="data">The data to assign to <paramref name="v"/>.</param>
+		/// <returns>Returns true if the data was set and false otherwise.</returns>
+		/// <exception cref="NoSuchVertexException">Thrown if <paramref name="v"/> does not exist in this graph.</exception>
+		/// <exception cref="NotImplementedException">Thrown if this graph does not support data alteration.</exception>
+		public bool SetVertexData(IVertex<V,E> v, V data);
+
+		/// <summary>
+		/// Sets the vertex data of vertex <paramref name="v"/> to <paramref name="data"/>.
+		/// </summary>
+		/// <param name="v">The vertex whose data should be set.</param>
+		/// <param name="data">The data to assign to <paramref name="v"/>.</param>
+		/// <returns>Returns true if the data was set and false otherwise.</returns>
+		public bool TrySetVertexData(IVertex<V,E> v, V data);
+
+		/// <summary>
+		/// Determines if this graph contains <paramref name="v"/>.
+		/// </summary>
+		/// <param name="v">The vertex to check for.</param>
+		/// <returns>Returns true if <paramref name="v"/> is present in this graph and false otherwise.</returns>
+		public bool ContainsVertex(IVertex<V,E> v);
+
+		/// <summary>
+		/// Adds an edge to this graph.
+		/// </summary>
+		/// <param name="src">The edge source vertex. If this graph is undirected, the order of <paramref name="src"/> and <paramref name="dst"/> is unimportant unless the implementing class explicitly specifies otherwise.</param>
+		/// <param name="dst">The edge destination vertex. If this graph is undirected, the order of <paramref name="src"/> and <paramref name="dst"/> is unimportant unless the implementing class explicitly specifies otherwise.</param>
+		/// <param name="data">The data to store in the edge.</param>
+		/// <returns>Returns the added edge.</returns>
+		public IEdge<V,E> AddEdge(IVertex<V,E> src, IVertex<V,E> dst, E data);
+
+		/// <summary>
+		/// Removes an edge from this graph.
+		/// </summary>
+		/// <param name="src">The edge source vertex. If this graph is undirected, the order of <paramref name="src"/> and <paramref name="dst"/> is unimportant unless the implementing class explicitly specifies otherwise.</param>
+		/// <param name="dst">The edge destination vertex. If this graph is undirected, the order of <paramref name="src"/> and <paramref name="dst"/> is unimportant unless the implementing class explicitly specifies otherwise.</param>
+		/// <returns>Returns true if the edge was removed and false otherwise.</returns>
+		/// <exception cref="NoSuchEdgeException">Thrown if the edge does not exist in this graph.</exception>
+		public bool RemoveEdge(IVertex<V,E> src, IVertex<V,E> dst);
+		
+		/// <summary>
+		/// Removes an edge from this graph.
+		/// </summary>
+		/// <param name="e">The edge to remove. If this graph is undirected, the direction of representation of <paramref name="e"/> is unimportant unless the implementing class explicitly specifies otherwise.</param>
+		/// <returns>Returns true if the edge was removed and false otherwise.</returns>
+		/// <exception cref="NoSuchEdgeException">Thrown if the edge does not exist in this graph.</exception>
+		public bool RemoveEdge(IEdge<V,E> e);
+		
+		/// <summary>
+		/// Removes an edge from this graph.
+		/// </summary>
+		/// <param name="src">The edge source vertex. If this graph is undirected, the order of <paramref name="src"/> and <paramref name="dst"/> is unimportant unless the implementing class explicitly specifies otherwise.</param>
+		/// <param name="dst">The edge destination vertex. If this graph is undirected, the order of <paramref name="src"/> and <paramref name="dst"/> is unimportant unless the implementing class explicitly specifies otherwise.</param>
+		/// <returns>Returns true if the edge was removed and false otherwise.</returns>
+		public bool TryRemoveEdge(IVertex<V,E> src, IVertex<V,E> dst);
+
+		/// <summary>
+		/// Removes an edge from this graph.
+		/// </summary>
+		/// <param name="e">The edge to remove. If this graph is undirected, the direction of representation of <paramref name="e"/> is unimportant unless the implementing class explicitly specifies otherwise.</param>
+		/// <returns>Returns true if the edge was removed and false otherwise.</returns>
+		public bool TryRemoveEdge(IEdge<V,E> e);
+
+		/// <summary>
+		/// Sets the edge data of an edge to <paramref name="data"/>.
+		/// </summary>
+		/// <param name="src">The edge source vertex. If this graph is undirected, the order of <paramref name="src"/> and <paramref name="dst"/> is unimportant unless the implementing class explicitly specifies otherwise.</param>
+		/// <param name="dst">The edge destination vertex. If this graph is undirected, the order of <paramref name="src"/> and <paramref name="dst"/> is unimportant unless the implementing class explicitly specifies otherwise.</param>
+		/// <param name="data">The data to assign to the edge.</param>
+		/// <returns>Returns true if the data was set and false otherwise.</returns>
+		/// <exception cref="NoSuchEdgeException">Thrown if the edge does not exist in this graph.</exception>
+		/// <exception cref="NotImplementedException">Thrown if this graph does not support data alteration.</exception>
+		public bool SetEdgeData(IVertex<V,E> src, IVertex<V,E> dst, E data);
+
+		/// <summary>
+		/// Sets the edge data of an edge to <paramref name="data"/>.
+		/// </summary>
+		/// <param name="e">The edge to remove. If this graph is undirected, the direction of representation of <paramref name="e"/> is unimportant unless the implementing class explicitly specifies otherwise.</param>
+		/// <param name="data">The data to assign to the edge.</param>
+		/// <returns>Returns true if the data was set and false otherwise.</returns>
+		/// <exception cref="NoSuchEdgeException">Thrown if the edge does not exist in this graph.</exception>
+		/// <exception cref="NotImplementedException">Thrown if this graph does not support data alteration.</exception>
+		public bool SetEdgeData(IEdge<V,E> e, E data);
+
+		/// <summary>
+		/// Sets the edge data of an edge.
+		/// </summary>
+		/// <param name="src">The edge source vertex. If this graph is undirected, the order of <paramref name="src"/> and <paramref name="dst"/> is unimportant unless the implementing class explicitly specifies otherwise.</param>
+		/// <param name="dst">The edge destination vertex. If this graph is undirected, the order of <paramref name="src"/> and <paramref name="dst"/> is unimportant unless the implementing class explicitly specifies otherwise.</param>
+		/// <param name="data">The data to assign to the edge.</param>
+		/// <returns>Returns true if the data was set and false otherwise.</returns>
+		public bool SetEdgeData(IVertex<V,E> src, IVertex<V,E> dst, V data);
+
+		/// <summary>
+		/// Sets the edge data of an edge.
+		/// </summary>
+		/// <param name="e">The edge to remove. If this graph is undirected, the direction of representation of <paramref name="e"/> is unimportant unless the implementing class explicitly specifies otherwise.</param>
+		/// <param name="data">The data to assign to the edge.</param>
+		/// <returns>Returns true if the data was set and false otherwise.</returns>
+		public bool SetEdgeData(IEdge<V,E> e, V data);
+
+		/// <summary>
+		/// Determines if this graph contains an edge.
+		/// </summary>
+		/// <param name="src">The edge source vertex. If this graph is undirected, the order of <paramref name="src"/> and <paramref name="dst"/> is unimportant unless the implementing class explicitly specifies otherwise.</param>
+		/// <param name="dst">The edge destination vertex. If this graph is undirected, the order of <paramref name="src"/> and <paramref name="dst"/> is unimportant unless the implementing class explicitly specifies otherwise.</param>
+		/// <returns>Returns true if the edge is present in this graph and false otherwise.</returns>
+		public bool ContainsEdge(IVertex<V,E> src, IVertex<V,E> dst);
+		
+		/// <summary>
+		/// Determines if this graph contains an edge.
+		/// </summary>
+		/// <param name="e">The edge to remove. If this graph is undirected, the direction of representation of <paramref name="e"/> is unimportant unless the implementing class explicitly specifies otherwise.</param>
+		/// <returns>Returns true if the edge is present in this graph and false otherwise.</returns>
+		public bool ContainsEdge(IEdge<V,E> e);
+
+		/// <summary>
+		/// Clears this graph.
+		/// </summary>
+		/// <exception cref="NotImplementedException">Thrown if this graph does not support removal.</exception>
+		public void Clear();
+
+		/// <summary>
+		/// The vertices of this graph.
+		/// </summary>
+		public IEnumerable<IVertex<V,E>> Vertices
+		{get;}
+		
+		/// <summary>
+		/// The number of vertices in this graph.
+		/// </summary>
+		public int VertexCount
+		{get;}
+
+		/// <summary>
+		/// The edges of this graph.
+		/// <para/>
+		/// If this is an undirected graph, each edge is listed only once.
+		/// In this case, each edge's direction of represention in undefined unless explicitly specified by an implementing class.
+		/// </summary>
+		public IEnumerable<IEdge<V,E>> Edges
+		{get;}
+
+		/// <summary>
+		/// The number of edges in this graph.
+		/// </summary>
+		public int EdgeCount
+		{get;}
+
+		/// <summary>
+		/// If true, then this graph is directed.
+		/// Otherwise, it is undirected.
+		/// </summary>
+		public bool Directed
+		{get;}
+
+		/// <summary>
+		/// If true, then this graph is undirected.
+		/// Otherwise, it is directed.
+		/// </summary>
+		public bool Undirected => !Directed;
+	}
+
+	/// <summary>
+	/// A basic vertex definition.
+	/// </summary>
+	/// <typeparam name="V">The data type stored in this vertex.</typeparam>
+	/// <typeparam name="E">The data type stored in edges connected to this vertex.</typeparam>
+	public interface IVertex<V,E>
+	{
+		/// <summary>
+		/// The neighbors of this vertex from inbound edges.
+		/// If this is an undirected graph, then this will contain the same set of vertices as OutboundNeighbors.
+		/// </summary>
+		/// <exception cref="NotImplementedException">Thrown if this vertex belongs to a directed graph and following edges in reverse is not permitted.</exception>
+		/// <remarks>If the graph structure is altered, all priorly generated versions of this have undefined behavior.</remarks>
+		public IEnumerable<IVertex<V,E>> InboundNeighbors
+		{get;}
+
+		/// <summary>
+		/// The in degree of this vertex.
+		/// </summary>
+		/// <exception cref="NotImplementedException">Thrown if this vertex belongs to a directed graph and following edges in reverse is not permitted.</exception>
+		public int InDegree
+		{get;}
+
+		/// <summary>
+		/// The neighbors of this vertex following outbound edges.
+		/// If this is an undirected graph, then this will contain the same set of vertices as InboundNeighbors.
+		/// </summary>
+		/// <remarks>If the graph structure is altered, all priorly generated versions of this have undefined behavior.</remarks>
+		public IEnumerable<IVertex<V,E>> OutboundNeighbors
+		{get;}
+
+		/// <summary>
+		/// The out degree of this vertex.
+		/// </summary>
+		public int OutDegree
+		{get;}
+
+		/// <summary>
+		/// The inbound edges to this vertex.
+		/// If this is an undirected graph, then this will contain the same set of edges as OutboundEdges.
+		/// </summary>
+		/// <exception cref="NotImplementedException">Thrown if this vertex belongs to a directed graph and following edges in reverse is not permitted.</exception>
+		/// <remarks>If the graph structure is altered, all priorly generated versions of this have undefined behavior.</remarks>
+		public IEnumerable<IEdge<V,E>> InboundEdges
+		{get;}
+
+		/// <summary>
+		/// The outbound edges from this vertex.
+		/// If this is an undirected graph, then this will contain the same set of edges as InboundEdges.
+		/// </summary>
+		/// <remarks>If the graph structure is altered, all priorly generated versions of this have undefined behavior.</remarks>
+		public IEnumerable<IEdge<V,E>> OutboundEdges
+		{get;}
+
+		/// <summary>
+		/// This vertex's data.
+		/// </summary>
+		public V Data
+		{get;}
+	}
+
+	/// <summary>
+	/// A basic edge definition representing the edge Source -> Destination.
+	/// If this is an undirected edge, then it also represents the edge Destination -> Source.
+	/// </summary>
+	/// <typeparam name="V">The data type stored in vertex endpoints of this edge.</typeparam>
+	/// <typeparam name="E">The data type stored in this edge.</typeparam>
+	public interface IEdge<V,E>
+	{
+		/// <summary>
+		/// The vertex this edge leaves.
+		/// </summary>
+		public IVertex<V,E> Source
+		{get;}
+
+		/// <summary>
+		/// The vertex this edge enters.
+		/// </summary>
+		public IVertex<V,E> Destination
+		{get;}
+
+		/// <summary>
+		/// If true, then this edge is directed.
+		/// Otherwise, it is undirected.
+		/// </summary>
+		public bool Directed
+		{get;}
+
+		/// <summary>
+		/// If true, then this edge is undirected.
+		/// Otherwise, it is directed.
+		/// </summary>
+		public bool Undirected => !Directed;
+
+		/// <summary>
+		/// This edge's data.
+		/// </summary>
+		public E Data
+		{get;}
+	}
+}
---
+++ GameEngine/Exceptions/IGraphExceptions.cs
@@ -0,0 +1,64 @@
+﻿using System.Runtime.Serialization;
+
+namespace GameEngine.Exceptions
+{
+	/// <summary>
+	/// An exception thrown when a vertex does not exist.
+	/// </summary>
+	public class NoSuchVertexException : Exception
+	{
+		/// <summary>
+		/// Creates a NoSuchVertexException.
+		/// </summary>
+		public NoSuchVertexException()
+		{return;}
+
+		/// <summary>
+		/// Creates a NoSuchVertexException with the provided message.
+		/// </summary>
+		public NoSuchVertexException(string? message) : base(message)
+		{return;}
+
+		/// <summary>
+		/// Creates a NoSuchVertexException with the provided message and inner exception.
+		/// </summary>
+		public NoSuchVertexException(string? message, Exception? innerException) : base(message,innerException)
+		{return;}
+
+		/// <summary>
+		/// Creates a NoSuchVertexException with the given serialization info and streaming context.
+		/// </summary>
+		public NoSuchVertexException(SerializationInfo info, StreamingContext context) : base(info,context)
+		{return;}
+	}
+
+	/// <summary>
+	/// An exception thrown when an edge does not exist.
+	/// </summary>
+	public class NoSuchEdgeException : Exception
+	{
+		/// <summary>
+		/// Creates a NoSuchEdgeException.
+		/// </summary>
+		public NoSuchEdgeException()
+		{return;}
+
+		/// <summary>
+		/// Creates a NoSuchEdgeException with the provided message.
+		/// </summary>
+		public NoSuchEdgeException(string? message) : base(message)
+		{return;}
+
+		/// <summary>
+		/// Creates a NoSuchEdgeException with the provided message and inner exception.
+		/// </summary>
+		public NoSuchEdgeException(string? message, Exception? innerException) : base(message,innerException)
+		{return;}
+
+		/// <summary>
+		/// Creates a NoSuchEdgeException with the given serialization info and streaming context.
+		/// </summary>
+		public NoSuchEdgeException(SerializationInfo info, StreamingContext context) : base(info,context)
+		{return;}
+	}
+}
