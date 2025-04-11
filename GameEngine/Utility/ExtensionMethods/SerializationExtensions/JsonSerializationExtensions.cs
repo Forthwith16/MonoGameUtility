ce0bc5756e11a9d2449dda15131dd2d15963b6ed
Forthwith16 <forthwith.dn@gmail.com>
2025-04-11 06:27:42 -0500

    Added json extensions.

--- GameEngine/Utility/ExtensionMethods/SerializationExtensions/JsonSerializationExtensions.cs
+++ GameEngine/Utility/ExtensionMethods/SerializationExtensions/JsonSerializationExtensions.cs
@@ -24,11 +24,39 @@ namespace GameEngine.Utility.ExtensionMethods.SerializationExtensions
 		/// </summary>
 		/// <typeparam name="T">The type to serialize.</typeparam>
 		/// <param name="value">The value to serialize.</param>
+		/// <param name="stream">The stream to write to.</param>
+		/// <param name="ops">The options ot pass to the serializer.</param>
+		public static void SerializeJson<T>(this T value, Stream stream, JsonSerializerOptions ops) where T : class
+		{
+			JsonSerializer.Serialize(stream,value,typeof(T),ops);
+			return;
+		}
+
+		/// <summary>
+		/// Serializes an object to a <paramref name="stream"/> in JSON format.
+		/// </summary>
+		/// <typeparam name="T">The type to serialize.</typeparam>
+		/// <param name="value">The value to serialize.</param>
 		/// <param name="path">The file to write to.</param>
 		public static void SerializeJson<T>(this T value, string path) where T : class
 		{
 			using(FileStream fout = File.Create(path))
 				value.SerializeJson(fout);
+
+			return;
+		}
+
+		/// <summary>
+		/// Serializes an object to a <paramref name="stream"/> in JSON format.
+		/// </summary>
+		/// <typeparam name="T">The type to serialize.</typeparam>
+		/// <param name="value">The value to serialize.</param>
+		/// <param name="path">The file to write to.</param>
+		/// <param name="ops">The options ot pass to the serializer.</param>
+		public static void SerializeJson<T>(this T value, string path, JsonSerializerOptions ops) where T : class
+		{
+			using(FileStream fout = File.Create(path))
+				value.SerializeJson(fout,ops);
 
 			return;
 		}
@@ -43,6 +71,23 @@ namespace GameEngine.Utility.ExtensionMethods.SerializationExtensions
 			using(MemoryStream sout = new MemoryStream())
 			{
 				value.SerializeJson(sout);
+
+				using(StreamReader sin = new StreamReader(sout))
+					return sin.ReadToEnd();
+			}
+		}
+
+		/// <summary>
+		/// Serializes an object to a string in JSON format.
+		/// </summary>
+		/// <typeparam name="T">The type to serialize.</typeparam>
+		/// <param name="value">The value to serialize.</param>
+		/// <param name="ops">The options ot pass to the serializer.</param>
+		public static string ToJsonString<T>(this T value, JsonSerializerOptions ops) where T : class
+		{
+			using(MemoryStream sout = new MemoryStream())
+			{
+				value.SerializeJson(sout,ops);
 
 				using(StreamReader sin = new StreamReader(sout))
 					return sin.ReadToEnd();
--- GameEngine/Utility/ExtensionMethods/SerializationExtensions/JsonSerializedStringExtensions.cs
+++ GameEngine/Utility/ExtensionMethods/SerializationExtensions/JsonSerializedStringExtensions.cs
@@ -23,6 +23,21 @@ namespace GameEngine.Utility.ExtensionMethods.SerializationExtensions
 		}
 
 		/// <summary>
+		/// Converts a JSON string into a <typeparamref name="T"/> type.
+		/// </summary>
+		/// <typeparam name="T">The type to convert to.</typeparam>
+		/// <param name="sjson">The JSON specification of a <typeparamref name="T"/> type.</param>
+		/// <param name="ops">The options ot pass to the serializer.</param>
+		/// <returns>Returns the converted object or null if <paramref name="sxml"/> was not a valid specification of a <typeparamref name="T"/> type.</returns>
+		public static T? DeserializeJson<T>(this string sjson, JsonSerializerOptions ops)
+		{
+			byte[] bytes = Encoding.GetEncoding(sjson).GetBytes(sjson);
+
+			using(MemoryStream sin = new MemoryStream(bytes))
+				return (T?)JsonSerializer.Deserialize(sin,typeof(T),ops);
+		}
+
+		/// <summary>
 		/// Converts a JSON file specified by <paramref name="path"/> into a <typeparamref name="T"/> type.
 		/// </summary>
 		/// <typeparam name="T">The type to convert to.</typeparam>
@@ -32,6 +47,19 @@ namespace GameEngine.Utility.ExtensionMethods.SerializationExtensions
 		{
 			using(FileStream sin = File.OpenRead(path))
 				return (T?)JsonSerializer.Deserialize(sin,typeof(T),JsonSerializationExtensions.SufficientReadWrite);
+		}
+
+		/// <summary>
+		/// Converts a JSON file specified by <paramref name="path"/> into a <typeparamref name="T"/> type.
+		/// </summary>
+		/// <typeparam name="T">The type to convert to.</typeparam>
+		/// <param name="path">The path to an JSON file specifying a <typeparamref name="T"/> type.</param>
+		/// <param name="ops">The options ot pass to the serializer.</param>
+		/// <returns>Returns the converted object or null if <paramref name="path"/> was not a valid file specifying a <typeparamref name="T"/> type.</returns>
+		public static T? DeserializeJsonFile<T>(this string path, JsonSerializerOptions ops)
+		{
+			using(FileStream sin = File.OpenRead(path))
+				return (T?)JsonSerializer.Deserialize(sin,typeof(T),ops);
 		}
 	}
 }
