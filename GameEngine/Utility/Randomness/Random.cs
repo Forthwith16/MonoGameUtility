42a86671c1d83844f4ad809add77ea1b694a625b
Dawn Nye <forthwith.dn@gmail.com>
2025-09-19 18:59:35 -0500

    Oops, bad namespace.

--- GameEngine/Utility/Randomness/Random.cs
+++ GameEngine/Utility/Randomness/Random.cs
@@ -2,7 +2,7 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace ZenChangYoEngine.Utility
+namespace GameEngine.Utility.Randomness
 {
 	/// <summary>
 	/// A unified implementation of random number generation
