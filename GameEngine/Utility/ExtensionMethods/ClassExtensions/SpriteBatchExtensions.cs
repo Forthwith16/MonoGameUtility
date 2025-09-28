041ce340c6c27e6cb0bba0e1174bdd6a077a61a8
Forthwith16 <forthwith.dn@gmail.com>
2025-09-28 13:18:09 -0700

    Oops, this should still be the base class.

--- GameEngine/Utility/ExtensionMethods/ClassExtensions/SpriteBatchExtensions.cs
+++ GameEngine/Utility/ExtensionMethods/ClassExtensions/SpriteBatchExtensions.cs
@@ -1,14 +1,13 @@
 ﻿using GameEngine.Maths;
-using GameEngine.Sprites;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace GameEngine.Utility.ExtensionMethods.ClassExtensions
 {
 	/// <summary>
-	/// Primitive drawing extensions for the SpriteRenderer class.
+	/// Primitive drawing extensions for the SpriteBatch class.
 	/// </summary>
-	public static class SpriteRendererExtensions
+	public static class SpriteBatchExtensions
 	{
 		/// <summary>
 		/// Draws a line from point <paramref name="a"/> to point <paramref name="b"/>.
@@ -19,7 +18,7 @@ namespace GameEngine.Utility.ExtensionMethods.ClassExtensions
 		/// <param name="tint">The color tint to draw the line texture with.</param>
 		/// <param name="thickness">The thickness of the line.</param>
 		/// <param name="layer_depth">The layer depth to draw at. This value should lie in the interval [0,1], where smaller values are considered the 'back' and larger values the 'front'.</param>
-		public static void DrawLine(this SpriteRenderer renderer, Vector2 a, Vector2 b, Color tint, float thickness, float layer_depth)
+		public static void DrawLine(this SpriteBatch renderer, Vector2 a, Vector2 b, Color tint, float thickness, float layer_depth)
 		{
 			// Remember that the y-axis points down in screen coordinates, so to get the right (oppositely signed) angle, we swap b.Y - a.Y to a.Y - b.Y
 			renderer.DrawLine(renderer.GetPixel(),a,(b - a).Length(),MathF.Atan2(a.Y - b.Y,b.X - a.X),tint,thickness,layer_depth);
@@ -37,7 +36,7 @@ namespace GameEngine.Utility.ExtensionMethods.ClassExtensions
 		/// <param name="tint">The color tint to draw the line texture with.</param>
 		/// <param name="thickness">The thickness of the line.</param>
 		/// <param name="layer_depth">The layer depth to draw at. This value should lie in the interval [0,1], where smaller values are considered the 'back' and larger values the 'front'.</param>
-		public static void DrawLine(this SpriteRenderer renderer, Vector2 p, float length, float angle, Color tint, float thickness, float layer_depth)
+		public static void DrawLine(this SpriteBatch renderer, Vector2 p, float length, float angle, Color tint, float thickness, float layer_depth)
 		{
 			renderer.DrawLine(renderer.GetPixel(),p,length,angle,tint,thickness,layer_depth);
 			return;
@@ -53,7 +52,7 @@ namespace GameEngine.Utility.ExtensionMethods.ClassExtensions
 		/// <param name="tint">The color tint to draw the line texture with.</param>
 		/// <param name="thickness">The thickness of the line.</param>
 		/// <param name="layer_depth">The layer depth to draw at. This value should lie in the interval [0,1], where smaller values are considered the 'back' and larger values the 'front'.</param>
-		public static void DrawLine(this SpriteRenderer renderer, Texture2D line_texture, Vector2 a, Vector2 b, Color tint, float thickness, float layer_depth)
+		public static void DrawLine(this SpriteBatch renderer, Texture2D line_texture, Vector2 a, Vector2 b, Color tint, float thickness, float layer_depth)
 		{
 			// Remember that the y-axis points down in screen coordinates, so to get the right (oppositely signed) angle, we swap b.Y - a.Y to a.Y - b.Y
 			renderer.DrawLine(line_texture,a,(b - a).Length(),MathF.Atan2(a.Y - b.Y,b.X - a.X),tint,thickness,layer_depth);
@@ -72,7 +71,7 @@ namespace GameEngine.Utility.ExtensionMethods.ClassExtensions
 		/// <param name="tint">The color tint to draw the line texture with.</param>
 		/// <param name="thickness">The thickness of the line.</param>
 		/// <param name="layer_depth">The layer depth to draw at. This value should lie in the interval [0,1], where smaller values are considered the 'back' and larger values the 'front'.</param>
-		public static void DrawLine(this SpriteRenderer renderer, Texture2D line_texture, Vector2 p, float length, float angle, Color tint, float thickness, float layer_depth)
+		public static void DrawLine(this SpriteBatch renderer, Texture2D line_texture, Vector2 p, float length, float angle, Color tint, float thickness, float layer_depth)
 		{
 			// Recall that angles are oppositely signed when it comes to screen coordinates in 2D, unfortunately
 			// Also, we are drawing the entire texture, so the scale factor needs to be scaled down by the side of the texture to get the correct dimensions
@@ -89,7 +88,7 @@ namespace GameEngine.Utility.ExtensionMethods.ClassExtensions
 		/// <param name="thickness">The thickness of the polygon.</param>
 		/// <param name="layer_depth">The layer depth to draw at. This value should lie in the interval [0,1], where smaller values are considered the 'back' and larger values the 'front'.</param>
 		/// <param name="points">The points of the polygon in clockwise or counterclockwise order. An extra line from the last to the first will be drawn to complete the polygon.</param>
-		public static void DrawPolygon(this SpriteRenderer renderer, Color tint, float thickness, float layer_depth, params Vector2[] points)
+		public static void DrawPolygon(this SpriteBatch renderer, Color tint, float thickness, float layer_depth, params Vector2[] points)
 		{
 			renderer.DrawPolygon(renderer.GetPixel(),tint,thickness,layer_depth,(IReadOnlyList<Vector2>)points);
 			return;
@@ -103,7 +102,7 @@ namespace GameEngine.Utility.ExtensionMethods.ClassExtensions
 		/// <param name="thickness">The thickness of the polygon.</param>
 		/// <param name="layer_depth">The layer depth to draw at. This value should lie in the interval [0,1], where smaller values are considered the 'back' and larger values the 'front'.</param>
 		/// <param name="points">The points of the polygon in clockwise or counterclockwise order. An extra line from the last to the first will be drawn to complete the polygon.</param>
-		public static void DrawPolygon(this SpriteRenderer renderer, Color tint, float thickness, float layer_depth, IReadOnlyList<Vector2> points)
+		public static void DrawPolygon(this SpriteBatch renderer, Color tint, float thickness, float layer_depth, IReadOnlyList<Vector2> points)
 		{
 			renderer.DrawPolygon(renderer.GetPixel(),tint,thickness,layer_depth,points);
 			return;
@@ -118,7 +117,7 @@ namespace GameEngine.Utility.ExtensionMethods.ClassExtensions
 		/// <param name="thickness">The thickness of the polygon.</param>
 		/// <param name="layer_depth">The layer depth to draw at. This value should lie in the interval [0,1], where smaller values are considered the 'back' and larger values the 'front'.</param>
 		/// <param name="points">The points of the polygon in clockwise or counterclockwise order. An extra line from the last to the first will be drawn to complete the polygon.</param>
-		public static void DrawPolygon(this SpriteRenderer renderer, Texture2D line_texture, Color tint, float thickness, float layer_depth, params Vector2[] points)
+		public static void DrawPolygon(this SpriteBatch renderer, Texture2D line_texture, Color tint, float thickness, float layer_depth, params Vector2[] points)
 		{
 			renderer.DrawPolygon(line_texture,tint,thickness,layer_depth,(IReadOnlyList<Vector2>)points);
 			return;
@@ -133,7 +132,7 @@ namespace GameEngine.Utility.ExtensionMethods.ClassExtensions
 		/// <param name="thickness">The thickness of the polygon.</param>
 		/// <param name="layer_depth">The layer depth to draw at. This value should lie in the interval [0,1], where smaller values are considered the 'back' and larger values the 'front'.</param>
 		/// <param name="points">The points of the polygon in clockwise or counterclockwise order. An extra line from the last to the first will be drawn to complete the polygon.</param>
-		public static void DrawPolygon(this SpriteRenderer renderer, Texture2D line_texture, Color tint, float thickness, float layer_depth, IReadOnlyList<Vector2> points)
+		public static void DrawPolygon(this SpriteBatch renderer, Texture2D line_texture, Color tint, float thickness, float layer_depth, IReadOnlyList<Vector2> points)
 		{
 			// If we don't have a polygon or even a line, do nothing
 			if(points.Count < 2)
@@ -161,7 +160,7 @@ namespace GameEngine.Utility.ExtensionMethods.ClassExtensions
 		/// <param name="thickness">The thickness of the polygon.</param>
 		/// <param name="layer_depth">The layer depth to draw at. This value should lie in the interval [0,1], where smaller values are considered the 'back' and larger values the 'front'.</param>
 		/// <param name="n">The number of sides to the n-gon.</param>
-		public static void DrawRegularNGon(this SpriteRenderer renderer, Vector2 center, float radius, float angle, Color tint, float thickness, float layer_depth, int n)
+		public static void DrawRegularNGon(this SpriteBatch renderer, Vector2 center, float radius, float angle, Color tint, float thickness, float layer_depth, int n)
 		{
 			renderer.DrawRegularNGon(renderer.GetPixel(),center,radius,angle,tint,thickness,layer_depth,n);
 			return;
@@ -179,7 +178,7 @@ namespace GameEngine.Utility.ExtensionMethods.ClassExtensions
 		/// <param name="thickness">The thickness of the polygon.</param>
 		/// <param name="layer_depth">The layer depth to draw at. This value should lie in the interval [0,1], where smaller values are considered the 'back' and larger values the 'front'.</param>
 		/// <param name="n">The number of sides to the n-gon.</param>
-		public static void DrawRegularNGon(this SpriteRenderer renderer, Texture2D line_texture, Vector2 center, float radius, float angle, Color tint, float thickness, float layer_depth, int n)
+		public static void DrawRegularNGon(this SpriteBatch renderer, Texture2D line_texture, Vector2 center, float radius, float angle, Color tint, float thickness, float layer_depth, int n)
 		{
 			// I don't know why you would want this, but just no
 			if(n < 3)
@@ -210,7 +209,7 @@ namespace GameEngine.Utility.ExtensionMethods.ClassExtensions
 		/// <param name="thickness">The thickness of the circle.</param>
 		/// <param name="layer_depth">The layer depth to draw at. This value should lie in the interval [0,1], where smaller values are considered the 'back' and larger values the 'front'.</param>
 		/// <param name="approximation_points">The number of points to draw the circle with. In practice, we draw a regular <paramref name="approximation_points"/>-gon to approximate the circle.</param>
-		public static void DrawCircle(this SpriteRenderer renderer, Vector2 center, float radius, Color tint, float thickness, float layer_depth, int approximation_points = 20)
+		public static void DrawCircle(this SpriteBatch renderer, Vector2 center, float radius, Color tint, float thickness, float layer_depth, int approximation_points = 20)
 		{
 			renderer.DrawCircle(renderer.GetPixel(),center,radius,tint,thickness,layer_depth,approximation_points);
 			return;
@@ -227,7 +226,7 @@ namespace GameEngine.Utility.ExtensionMethods.ClassExtensions
 		/// <param name="thickness">The thickness of the circle.</param>
 		/// <param name="layer_depth">The layer depth to draw at. This value should lie in the interval [0,1], where smaller values are considered the 'back' and larger values the 'front'.</param>
 		/// <param name="approximation_points">The number of points to draw the circle with. In practice, we draw a regular <paramref name="approximation_points"/>-gon to approximate the circle.</param>
-		public static void DrawCircle(this SpriteRenderer renderer, Texture2D line_texture, Vector2 center, float radius, Color tint, float thickness, float layer_depth, int approximation_points = 20)
+		public static void DrawCircle(this SpriteBatch renderer, Texture2D line_texture, Vector2 center, float radius, Color tint, float thickness, float layer_depth, int approximation_points = 20)
 		{
 			// I don't know why you would want this, but just no
 			if(approximation_points < 3)
@@ -251,7 +250,7 @@ namespace GameEngine.Utility.ExtensionMethods.ClassExtensions
 		/// <summary>
 		/// Produces a (singleton) single pixel used to draw stuff.
 		/// </summary>
-		private static Texture2D GetPixel(this SpriteRenderer renderer)
+		private static Texture2D GetPixel(this SpriteBatch renderer)
 		{
 			if(_dp is null)
 			{
