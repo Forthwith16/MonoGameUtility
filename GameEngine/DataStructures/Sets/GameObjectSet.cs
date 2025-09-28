93b901cfb8bcfc8e0854412334bbe283d23d9d3f
Forthwith16 <forthwith.dn@gmail.com>
2025-09-27 21:34:54 -0700

    Made GameObjectSets the only game in town.

--- GameEngine/DataStructures/Sets/GameObjectSet.cs
+++ GameEngine/DataStructures/Sets/GameObjectSet.cs
@@ -1,5 +1,7 @@
 ﻿using GameEngine.Framework;
+using GameEngine.GameObjects;
 using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 
 namespace GameEngine.DataStructures.Sets
 {
@@ -20,7 +22,7 @@ namespace GameEngine.DataStructures.Sets
 		public GameObjectSet(RenderTargetFriendlyGame game)
 		{
 			Game = game;
-			Storage = new AVLSet<T>(Comparer<T>.Create((a,b) => a.ID.CompareTo(b.ID)));
+			Storage = new AVLSet<GameObject>(Comparer<GameObject>.Create((a,b) => a.ID.CompareTo(b.ID)));
 
 			InGame = false;
 			return;
@@ -46,7 +48,7 @@ namespace GameEngine.DataStructures.Sets
 		}
 
 		/// <summary>
-		/// Remvoes this set's components from <see cref="Game"/>.
+		/// Removes this set's components from <see cref="Game"/>.
 		/// </summary>
 		public void RemoveAllFromGame()
 		{
@@ -69,12 +71,38 @@ namespace GameEngine.DataStructures.Sets
 			return;
 		}
 
-		public bool Remove(T item)
+		public bool Remove(T item) => SecretRemove(item);
+
+		/// <summary>
+		/// A private version of <see cref="Remove(T)"/> that does not care about type <typeparamref name="T"/>.
+		/// </summary>
+		/// <param name="obj">The game object to remove.</param>
+		/// <returns>Returns true if the remove was successful and false otherwise.</returns>
+		private bool SecretRemove(GameObject obj)
 		{
 			if(InGame)
-				Game.Components.Remove(item);
+				Game.Components.Remove(obj);
 
-			return Storage.Remove(item);
+			return Storage.Remove(obj);
+		}
+
+		/// <summary>
+		/// Obtains an object by its ID.
+		/// </summary>
+		/// <param name="id">The ID of the object to fetch.</param>
+		/// <param name="output">The output variable. This will be the game object with ID <paramref name="id"/> when this returns true and default(<typeparamref name="T"/>) otherwise.</param>
+		/// <returns>Returns true if this contained the game object, which was assigned to <paramref name="output"/>, and false otherwise.</returns>
+		/// <remarks>This method requires that we can search for <typeparamref name="T"/> types by their ID by creating a DummyGameObject with ID <paramref name="id"/>.</remarks>
+		public bool TryGet(GameObjectID id, [MaybeNullWhen(false)] out T output)
+		{
+			if(Storage.Get(new DummyGameObject(id),out GameObject? obj))
+			{
+				output = obj as T;
+				return output is not null;
+			}
+
+			output = null;
+			return false;
 		}
 
 		public bool Contains(T item) => Storage.Contains(item);
@@ -82,14 +110,28 @@ namespace GameEngine.DataStructures.Sets
 		public void Clear()
 		{
 			while(NotEmpty)
-				Remove(Storage.First());
+				SecretRemove(Storage.First());
 			
 			return;
 		}
 
-		public void CopyTo(T[] array, int offset) => Storage.CopyTo(array,offset);
+		public void CopyTo(T[] array, int offset)
+		{
+			if(offset < 0)
+				throw new ArgumentOutOfRangeException();
 
-		public IEnumerator<T> GetEnumerator() => Storage.GetEnumerator();
+			if(array.Length < offset + Count)
+				throw new ArgumentException();
+
+			int i = 0;
+
+			foreach(T t in this)
+				array[offset + i++] = t;
+
+			return;
+		}
+
+		public IEnumerator<T> GetEnumerator() => Storage.Select(obj => (T)obj).GetEnumerator();
 		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
 		/// <summary>
@@ -108,7 +150,7 @@ namespace GameEngine.DataStructures.Sets
 		/// <summary>
 		/// The way we store objects.
 		/// </summary>
-		protected AVLSet<T> Storage
+		protected AVLSet<GameObject> Storage
 		{get;}
 
 		/// <summary>
---
+++ GameEngine/GameObjects/DummyGameObject.cs
@@ -0,0 +1,23 @@
+﻿using GameEngine.Framework;
+
+namespace GameEngine.GameObjects
+{
+	/// <summary>
+	/// A ultra lightweight dummy game object whose only purpose is to exist.
+	/// </summary>
+	public sealed class DummyGameObject : GameObject
+	{
+		/// <summary>
+		/// Creates a dummy game object with all default values.
+		/// </summary>
+		public DummyGameObject() : base()
+		{return;}
+
+		/// <summary>
+		/// Creates a dummy game object with the given ID.
+		/// This is particularly useful for searching for game objects by ID, since game object equality is checked via ID.
+		/// </summary>
+		public DummyGameObject(GameObjectID id) : base(id)
+		{return;}
+	}
+}
