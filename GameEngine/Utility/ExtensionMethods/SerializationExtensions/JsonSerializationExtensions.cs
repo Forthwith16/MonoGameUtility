625db776bdc2cf708d24c1aee86a244605c6ecae
Forthwith16 <forthwith.dn@gmail.com>
2025-10-07 22:47:22 -0700

    Improved JSON serialization/deserialization documentation and expanded the set of methods available to be more complete. Also added support for singleton converters so that we can convert to/from JSON in a thread safe manner while having state information exclusive to a single object. This is technically not needed but saves us from having to make and manage a dictionary of state information.

--- GameEngine/Utility/ExtensionMethods/SerializationExtensions/JsonSerializationExtensions.cs
+++ GameEngine/Utility/ExtensionMethods/SerializationExtensions/JsonSerializationExtensions.cs
@@ -8,17 +8,14 @@ namespace GameEngine.Utility.ExtensionMethods.SerializationExtensions
 	/// </summary>
 	public static class JsonSerializationExtensions
 	{
+		#region Streams
 		/// <summary>
 		/// Serializes an object to a <paramref name="stream"/> in JSON format.
 		/// </summary>
 		/// <typeparam name="T">The type to serialize.</typeparam>
 		/// <param name="value">The value to serialize.</param>
 		/// <param name="stream">The stream to write to.</param>
-		public static void SerializeJson<T>(this T value, Stream stream)
-		{
-			JsonSerializer.Serialize(stream,value,typeof(T),SufficientReadWrite);
-			return;
-		}
+		public static void SerializeJson<T>(this T value, Stream stream) => value.SerializeJson(stream,SufficientReadWrite);
 
 		/// <summary>
 		/// Serializes an object to a <paramref name="stream"/> in JSON format.
@@ -26,10 +23,18 @@ namespace GameEngine.Utility.ExtensionMethods.SerializationExtensions
 		/// <typeparam name="T">The type to serialize.</typeparam>
 		/// <param name="value">The value to serialize.</param>
 		/// <param name="stream">The stream to write to.</param>
-		/// <param name="ops">The options ot pass to the serializer.</param>
-		public static void SerializeJson<T>(this T value, Stream stream, JsonSerializerOptions ops)
+		/// <param name="converter">
+		/// The converter to use for the serialization.
+		/// This will be added to the JsonSerializerOptions's set of converters.
+		/// This will take precedence over the default <typeparamref name="T"/> type converter but will lose precedence to a property with a custom JsonConverter attribute.
+		/// </param>
+		/// <remarks>The strength of this method is that it allows for multiple truly independent conversions of a type to occur simultaneously, allowing for converters to contain member state information exclusive to the object being converted.</remarks>
+		public static void ToJsonString<T>(this T value, Stream stream, JsonConverter<T> converter)
 		{
-			JsonSerializer.Serialize(stream,value,typeof(T),ops);
+			JsonSerializerOptions ops = new JsonSerializerOptions(SufficientReadWrite);
+			ops.Converters.Add(converter);
+
+			value.SerializeJson(stream,ops);
 			return;
 		}
 
@@ -38,12 +43,19 @@ namespace GameEngine.Utility.ExtensionMethods.SerializationExtensions
 		/// </summary>
 		/// <typeparam name="T">The type to serialize.</typeparam>
 		/// <param name="value">The value to serialize.</param>
-		/// <param name="path">The file to write to.</param>
-		public static void SerializeJson<T>(this T value, string path)
+		/// <param name="stream">The stream to write to.</param>
+		/// <param name="converter">
+		/// The converter factory to use for the serialization.
+		/// This will be added to the JsonSerializerOptions's set of converters.
+		/// This will take precedence over the default <typeparamref name="T"/> type converter but will lose precedence to a property with a custom JsonConverter attribute.
+		/// </param>
+		/// <remarks>The strength of this method is that it allows for multiple truly independent conversions of a type to occur simultaneously, allowing for converters to contain member state information exclusive to the object being converted.</remarks>
+		public static void ToJsonString<T>(this T value, Stream stream, JsonConverterFactory converter_factory)
 		{
-			using(FileStream fout = File.Create(path))
-				value.SerializeJson(fout);
+			JsonSerializerOptions ops = new JsonSerializerOptions(SufficientReadWrite);
+			ops.Converters.Add(converter_factory);
 
+			value.SerializeJson(stream,ops);
 			return;
 		}
 
@@ -52,8 +64,69 @@ namespace GameEngine.Utility.ExtensionMethods.SerializationExtensions
 		/// </summary>
 		/// <typeparam name="T">The type to serialize.</typeparam>
 		/// <param name="value">The value to serialize.</param>
+		/// <param name="stream">The stream to write to.</param>
+		/// <param name="ops">The options to pass to the serializer.</param>
+		public static void SerializeJson<T>(this T value, Stream stream, JsonSerializerOptions ops) => JsonSerializer.Serialize(stream,value,typeof(T),ops);
+		#endregion
+
+		#region Files
+		/// <summary>
+		/// Serializes an object to a file at <paramref name="path"/> in JSON format.
+		/// </summary>
+		/// <typeparam name="T">The type to serialize.</typeparam>
+		/// <param name="value">The value to serialize.</param>
 		/// <param name="path">The file to write to.</param>
-		/// <param name="ops">The options ot pass to the serializer.</param>
+		public static void SerializeJson<T>(this T value, string path) => value.SerializeJson(path,SufficientReadWrite);
+
+		/// <summary>
+		/// Serializes an object to a file at <paramref name="path"/> in JSON format.
+		/// </summary>
+		/// <typeparam name="T">The type to serialize.</typeparam>
+		/// <param name="value">The value to serialize.</param>
+		/// <param name="path">The file to write to.</param>
+		/// <param name="converter">
+		/// The converter to use for the serialization.
+		/// This will be added to the JsonSerializerOptions's set of converters.
+		/// This will take precedence over the default <typeparamref name="T"/> type converter but will lose precedence to a property with a custom JsonConverter attribute.
+		/// </param>
+		/// <remarks>The strength of this method is that it allows for multiple truly independent conversions of a type to occur simultaneously, allowing for converters to contain member state information exclusive to the object being converted.</remarks>
+		public static void ToJsonString<T>(this T value, string path, JsonConverter<T> converter)
+		{
+			JsonSerializerOptions ops = new JsonSerializerOptions(SufficientReadWrite);
+			ops.Converters.Add(converter);
+
+			value.SerializeJson(path,ops);
+			return;
+		}
+
+		/// <summary>
+		/// Serializes an object to a file at <paramref name="path"/> in JSON format.
+		/// </summary>
+		/// <typeparam name="T">The type to serialize.</typeparam>
+		/// <param name="value">The value to serialize.</param>
+		/// <param name="path">The file to write to.</param>
+		/// <param name="converter">
+		/// The converter factory to use for the serialization.
+		/// This will be added to the JsonSerializerOptions's set of converters.
+		/// This will take precedence over the default <typeparamref name="T"/> type converter but will lose precedence to a property with a custom JsonConverter attribute.
+		/// </param>
+		/// <remarks>The strength of this method is that it allows for multiple truly independent conversions of a type to occur simultaneously, allowing for converters to contain member state information exclusive to the object being converted.</remarks>
+		public static void ToJsonString<T>(this T value, string path, JsonConverterFactory converter_factory)
+		{
+			JsonSerializerOptions ops = new JsonSerializerOptions(SufficientReadWrite);
+			ops.Converters.Add(converter_factory);
+
+			value.SerializeJson(path,ops);
+			return;
+		}
+
+		/// <summary>
+		/// Serializes an object to a file at <paramref name="path"/> in JSON format.
+		/// </summary>
+		/// <typeparam name="T">The type to serialize.</typeparam>
+		/// <param name="value">The value to serialize.</param>
+		/// <param name="path">The file to write to.</param>
+		/// <param name="ops">The options to pass to the serializer.</param>
 		public static void SerializeJson<T>(this T value, string path, JsonSerializerOptions ops)
 		{
 			using(FileStream fout = File.Create(path))
@@ -61,22 +134,34 @@ namespace GameEngine.Utility.ExtensionMethods.SerializationExtensions
 
 			return;
 		}
+		#endregion
+
+		#region Strings
+		/// <summary>
+		/// Serializes an object to a string in JSON format.
+		/// </summary>
+		/// <typeparam name="T">The type to serialize.</typeparam>
+		/// <param name="value">The value to serialize.</param>
+		public static string ToJsonString<T>(this T value) => value.ToJsonString(SufficientReadWrite);
 
 		/// <summary>
 		/// Serializes an object to a string in JSON format.
 		/// </summary>
 		/// <typeparam name="T">The type to serialize.</typeparam>
 		/// <param name="value">The value to serialize.</param>
-		public static string ToJsonString<T>(this T value)
+		/// <param name="converter">
+		/// The converter to use for the serialization.
+		/// This will be added to the JsonSerializerOptions's set of converters.
+		/// This will take precedence over the default <typeparamref name="T"/> type converter but will lose precedence to a property with a custom JsonConverter attribute.
+		/// </param>
+		/// <returns>Returns the resulting JSON string.</returns>
+		/// <remarks>The strength of this method is that it allows for multiple truly independent conversions of a type to occur simultaneously, allowing for converters to contain member state information exclusive to the object being converted.</remarks>
+		public static string ToJsonString<T>(this T value, JsonConverter<T> converter)
 		{
-			using(MemoryStream sout = new MemoryStream())
-			{
-				value.SerializeJson(sout);
-				sout.Position = 0;
+			JsonSerializerOptions ops = new JsonSerializerOptions(SufficientReadWrite);
+			ops.Converters.Add(converter);
 
-				using(StreamReader sin = new StreamReader(sout))
-					return sin.ReadToEnd();
-			}
+			return value.ToJsonString(ops);
 		}
 
 		/// <summary>
@@ -84,47 +169,36 @@ namespace GameEngine.Utility.ExtensionMethods.SerializationExtensions
 		/// </summary>
 		/// <typeparam name="T">The type to serialize.</typeparam>
 		/// <param name="value">The value to serialize.</param>
-		/// <param name="ops">The options ot pass to the serializer.</param>
-		public static string ToJsonString<T>(this T value, JsonSerializerOptions ops)
+		/// <param name="converter">
+		/// The converter factory to use for the serialization.
+		/// This will be added to the JsonSerializerOptions's set of converters.
+		/// This will take precedence over the default <typeparamref name="T"/> type converter but will lose precedence to a property with a custom JsonConverter attribute.
+		/// </param>
+		/// <returns>Returns the resulting JSON string.</returns>
+		/// <remarks>The strength of this method is that it allows for multiple truly independent conversions of a type to occur simultaneously, allowing for converters to contain member state information exclusive to the object being converted.</remarks>
+		public static string ToJsonString<T>(this T value, JsonConverterFactory converter_factory)
 		{
-			using(MemoryStream sout = new MemoryStream())
-			{
-				value.SerializeJson(sout,ops);
-				sout.Position = 0;
-				
-				using(StreamReader sin = new StreamReader(sout))
-					return sin.ReadToEnd();
-			}
+			JsonSerializerOptions ops = new JsonSerializerOptions(SufficientReadWrite);
+			ops.Converters.Add(converter_factory);
+
+			return value.ToJsonString(ops);
 		}
 
 		/// <summary>
-		/// Adds a converter to the default set of JSON options.
+		/// Serializes an object to a string in JSON format.
 		/// </summary>
-		/// <param name="c">The converter to add.</param>
-		public static void RegisterConverter(JsonConverter c)
-		{
-			SufficientReadWrite = new JsonSerializerOptions(SufficientReadWrite); // This makes it so we can change options even after having serialized things
-			SufficientReadWrite.Converters.Add(c);
-			
-			return;
-		}
+		/// <typeparam name="T">The type to serialize.</typeparam>
+		/// <param name="value">The value to serialize.</param>
+		/// <param name="ops">The options to pass to the serializer.</param>
+		public static string ToJsonString<T>(this T value, JsonSerializerOptions ops) => JsonSerializer.Serialize(value,typeof(T),ops);
+		#endregion
 
 		/// <summary>
-		/// Removes a converter from the default set of JSON options.
+		/// The basic JSON serialization/deserialization options.
+		/// This will pretty print and ignore readonly fields and properties by default, which cannot be assigned after construction time.
 		/// </summary>
-		/// <param name="c">The converter to remove.</param>
-		public static void UnregisterConverter(JsonConverter c)
-		{
-			SufficientReadWrite = new JsonSerializerOptions(SufficientReadWrite); // This makes it so we can change options even after having serialized things
-			SufficientReadWrite.Converters.Remove(c);
-			
-			return;
-		}
-
-		/// <summary>
-		/// The JSON options to serialize everything that needs to be serialized to recover the full variable state later.
-		/// </summary>
-		internal static JsonSerializerOptions SufficientReadWrite = new JsonSerializerOptions
+		internal static JsonSerializerOptions SufficientReadWrite
+		{get;} = new JsonSerializerOptions
 		{
 			IncludeFields = true,
 			IgnoreReadOnlyFields = true,
--- GameEngine/Utility/ExtensionMethods/SerializationExtensions/JsonSerializedStringExtensions.cs
+++ GameEngine/Utility/ExtensionMethods/SerializationExtensions/JsonSerializedStringExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace GameEngine.Utility.ExtensionMethods.SerializationExtensions
 {
@@ -7,33 +8,89 @@ namespace GameEngine.Utility.ExtensionMethods.SerializationExtensions
 	/// </summary>
 	public static class JsonSerializedStringExtensions
 	{
+		#region Streams
 		/// <summary>
-		/// Converts a JSON string into a <typeparamref name="T"/> type.
+		/// Converts a JSON object held in a stream into a <typeparamref name="T"/> type.
 		/// </summary>
 		/// <typeparam name="T">The type to convert to.</typeparam>
-		/// <param name="sjson">The JSON specification of a <typeparamref name="T"/> type.</param>
-		/// <returns>Returns the converted object or null if <paramref name="sxml"/> was not a valid specification of a <typeparamref name="T"/> type.</returns>
-		public static T? DeserializeJson<T>(this string sjson) => (T?)JsonSerializer.Deserialize(sjson,typeof(T),JsonSerializationExtensions.SufficientReadWrite);
+		/// <param name="stream">The stream to read from containing a JSON serialized object of type <typeparamref name="T"/>.</param>
+		/// <returns>Returns the converted object or null if <paramref name="stream"/> did not contain a valid JSON serialization of a <typeparamref name="T"/> type.</returns>
+		public static T? DeserializeJson<T>(this Stream stream) => JsonSerializer.Deserialize<T>(stream,JsonSerializationExtensions.SufficientReadWrite);
 
 		/// <summary>
-		/// Converts a JSON string into a <typeparamref name="T"/> type.
+		/// Converts a JSON object held in a stream into a <typeparamref name="T"/> type.
 		/// </summary>
 		/// <typeparam name="T">The type to convert to.</typeparam>
-		/// <param name="sjson">The JSON specification of a <typeparamref name="T"/> type.</param>
-		/// <param name="ops">The options ot pass to the serializer.</param>
-		/// <returns>Returns the converted object or null if <paramref name="sxml"/> was not a valid specification of a <typeparamref name="T"/> type.</returns>
-		public static T? DeserializeJson<T>(this string sjson, JsonSerializerOptions ops) => (T?)JsonSerializer.Deserialize(sjson,typeof(T),ops);
+		/// <param name="stream">The stream to read from containing a JSON serialized object of type <typeparamref name="T"/>.</param>
+		/// <param name="converter">
+		/// The converter to use for the deserialization.
+		/// This will be added to the JsonSerializerOptions's set of converters.
+		/// This will take precedence over the default <typeparamref name="T"/> type converter but will lose precedence to a property with a custom JsonConverter attribute.
+		/// </param>
+		/// <remarks>The strength of this method is that it allows for multiple truly independent conversions of a type to occur simultaneously, allowing for converters to contain member state information exclusive to the object being converted.</remarks>
+		public static T? DeserializeJson<T>(this Stream stream, JsonConverter<T> converter)
+		{
+			JsonSerializerOptions ops = new JsonSerializerOptions(JsonSerializationExtensions.SufficientReadWrite);
+			ops.Converters.Add(converter);
 
+			return stream.DeserializeJson<T>(ops);
+		}
+
+		/// <summary>
+		/// Converts a JSON object held in a stream into a <typeparamref name="T"/> type.
+		/// </summary>
+		/// <typeparam name="T">The type to convert to.</typeparam>
+		/// <param name="stream">The stream to read from containing a JSON serialized object of type <typeparamref name="T"/>.</param>
+		/// <param name="converter">
+		/// The converter factory to use for the deserialization.
+		/// This will be added to the JsonSerializerOptions's set of converters.
+		/// This will take precedence over the default <typeparamref name="T"/> type converter but will lose precedence to a property with a custom JsonConverter attribute.
+		/// </param>
+		/// <remarks>The strength of this method is that it allows for multiple truly independent conversions of a type to occur simultaneously, allowing for converters to contain member state information exclusive to the object being converted.</remarks>
+		public static T? DeserializeJson<T>(this Stream stream, JsonConverterFactory converter_factory)
+		{
+			JsonSerializerOptions ops = new JsonSerializerOptions(JsonSerializationExtensions.SufficientReadWrite);
+			ops.Converters.Add(converter_factory);
+
+			return stream.DeserializeJson<T>(ops);
+		}
+
+		/// <summary>
+		/// Converts a JSON object held in a stream into a <typeparamref name="T"/> type.
+		/// </summary>
+		/// <typeparam name="T">The type to convert to.</typeparam>
+		/// <param name="stream">The stream to read from containing a JSON serialized object of type <typeparamref name="T"/>.</param>
+		/// <param name="ops">The options to pass to the serializer.</param>
+		/// <returns>Returns the converted object or null if <paramref name="stream"/> did not contain a valid JSON serialization of a <typeparamref name="T"/> type.</returns>
+		public static T? DeserializeJson<T>(this Stream stream, JsonSerializerOptions ops) => JsonSerializer.Deserialize<T>(stream,ops);
+		#endregion
+
+		#region Files
 		/// <summary>
 		/// Converts a JSON file specified by <paramref name="path"/> into a <typeparamref name="T"/> type.
 		/// </summary>
 		/// <typeparam name="T">The type to convert to.</typeparam>
 		/// <param name="path">The path to an JSON file specifying a <typeparamref name="T"/> type.</param>
 		/// <returns>Returns the converted object or null if <paramref name="path"/> was not a valid file specifying a <typeparamref name="T"/> type.</returns>
-		public static T? DeserializeJsonFile<T>(this string path)
+		public static T? DeserializeJsonFile<T>(this string path) => path.DeserializeJsonFile<T>(JsonSerializationExtensions.SufficientReadWrite);
+
+		/// <summary>
+		/// Converts a JSON file specified by <paramref name="path"/> into a <typeparamref name="T"/> type.
+		/// </summary>
+		/// <typeparam name="T">The type to convert to.</typeparam>
+		/// <param name="path">The path to an JSON file specifying a <typeparamref name="T"/> type.</param>
+		/// <param name="converter">
+		/// The converter to use for the deserialization.
+		/// This will be added to the JsonSerializerOptions's set of converters.
+		/// This will take precedence over the default <typeparamref name="T"/> type converter but will lose precedence to a property with a custom JsonConverter attribute.
+		/// </param>
+		/// <remarks>The strength of this method is that it allows for multiple truly independent conversions of a type to occur simultaneously, allowing for converters to contain member state information exclusive to the object being converted.</remarks>
+		public static T? DeserializeJsonFile<T>(this string path, JsonConverter<T> converter)
 		{
-			using(FileStream sin = File.OpenRead(path))
-				return (T?)JsonSerializer.Deserialize(sin,typeof(T),JsonSerializationExtensions.SufficientReadWrite);
+			JsonSerializerOptions ops = new JsonSerializerOptions(JsonSerializationExtensions.SufficientReadWrite);
+			ops.Converters.Add(converter);
+
+			return path.DeserializeJsonFile<T>(ops);
 		}
 
 		/// <summary>
@@ -41,12 +98,89 @@ namespace GameEngine.Utility.ExtensionMethods.SerializationExtensions
 		/// </summary>
 		/// <typeparam name="T">The type to convert to.</typeparam>
 		/// <param name="path">The path to an JSON file specifying a <typeparamref name="T"/> type.</param>
-		/// <param name="ops">The options ot pass to the serializer.</param>
+		/// <param name="converter">
+		/// The converter factory to use for the deserialization.
+		/// This will be added to the JsonSerializerOptions's set of converters.
+		/// This will take precedence over the default <typeparamref name="T"/> type converter but will lose precedence to a property with a custom JsonConverter attribute.
+		/// </param>
+		/// <remarks>The strength of this method is that it allows for multiple truly independent conversions of a type to occur simultaneously, allowing for converters to contain member state information exclusive to the object being converted.</remarks>
+		public static T? DeserializeJsonFile<T>(this string path, JsonConverterFactory converter_factory)
+		{
+			JsonSerializerOptions ops = new JsonSerializerOptions(JsonSerializationExtensions.SufficientReadWrite);
+			ops.Converters.Add(converter_factory);
+
+			return path.DeserializeJsonFile<T>(ops);
+		}
+
+		/// <summary>
+		/// Converts a JSON file specified by <paramref name="path"/> into a <typeparamref name="T"/> type.
+		/// </summary>
+		/// <typeparam name="T">The type to convert to.</typeparam>
+		/// <param name="path">The path to an JSON file specifying a <typeparamref name="T"/> type.</param>
+		/// <param name="ops">The options to pass to the serializer.</param>
 		/// <returns>Returns the converted object or null if <paramref name="path"/> was not a valid file specifying a <typeparamref name="T"/> type.</returns>
 		public static T? DeserializeJsonFile<T>(this string path, JsonSerializerOptions ops)
 		{
 			using(FileStream sin = File.OpenRead(path))
-				return (T?)JsonSerializer.Deserialize(sin,typeof(T),ops);
+				return sin.DeserializeJson<T>(ops);
 		}
+		#endregion
+
+		#region Strings
+		/// <summary>
+		/// Converts a JSON string into a <typeparamref name="T"/> type.
+		/// </summary>
+		/// <typeparam name="T">The type to convert to.</typeparam>
+		/// <param name="sjson">The JSON serialization of a <typeparamref name="T"/> type.</param>
+		/// <returns>Returns the converted object or null if <paramref name="sjson"/> was not a valid specification of a <typeparamref name="T"/> type.</returns>
+		public static T? DeserializeJson<T>(this string sjson) => sjson.DeserializeJson<T>(JsonSerializationExtensions.SufficientReadWrite);
+
+		/// <summary>
+		/// Converts a JSON string into a <typeparamref name="T"/> type.
+		/// </summary>
+		/// <typeparam name="T">The type to convert to.</typeparam>
+		/// <param name="sjson">The JSON serialization of a <typeparamref name="T"/> type.</param>
+		/// <param name="converter">
+		/// The converter to use for the deserialization.
+		/// This will be added to the JsonSerializerOptions's set of converters.
+		/// This will take precedence over the default <typeparamref name="T"/> type converter but will lose precedence to a property with a custom JsonConverter attribute.
+		/// </param>
+		/// <remarks>The strength of this method is that it allows for multiple truly independent conversions of a type to occur simultaneously, allowing for converters to contain member state information exclusive to the object being converted.</remarks>
+		public static T? DeserializeJson<T>(this string sjson, JsonConverter<T> converter)
+		{
+			JsonSerializerOptions ops = new JsonSerializerOptions(JsonSerializationExtensions.SufficientReadWrite);
+			ops.Converters.Add(converter);
+
+			return sjson.DeserializeJson<T>(ops);
+		}
+
+		/// <summary>
+		/// Converts a JSON string into a <typeparamref name="T"/> type.
+		/// </summary>
+		/// <typeparam name="T">The type to convert to.</typeparam>
+		/// <param name="sjson">The JSON serialization of a <typeparamref name="T"/> type.</param>
+		/// <param name="converter">
+		/// The converter factory to use for the deserialization.
+		/// This will be added to the JsonSerializerOptions's set of converters.
+		/// This will take precedence over the default <typeparamref name="T"/> type converter but will lose precedence to a property with a custom JsonConverter attribute.
+		/// </param>
+		/// <remarks>The strength of this method is that it allows for multiple truly independent conversions of a type to occur simultaneously, allowing for converters to contain member state information exclusive to the object being converted.</remarks>
+		public static T? DeserializeJson<T>(this string sjson, JsonConverterFactory converter_factory)
+		{
+			JsonSerializerOptions ops = new JsonSerializerOptions(JsonSerializationExtensions.SufficientReadWrite);
+			ops.Converters.Add(converter_factory);
+
+			return sjson.DeserializeJson<T>(ops);
+		}
+
+		/// <summary>
+		/// Converts a JSON string into a <typeparamref name="T"/> type.
+		/// </summary>
+		/// <typeparam name="T">The type to convert to.</typeparam>
+		/// <param name="sjson">The JSON serialization of a <typeparamref name="T"/> type.</param>
+		/// <param name="ops">The options to pass to the serializer.</param>
+		/// <returns>Returns the converted object or null if <paramref name="sjson"/> was not a valid specification of a <typeparamref name="T"/> type.</returns>
+		public static T? DeserializeJson<T>(this string sjson, JsonSerializerOptions ops) => JsonSerializer.Deserialize<T>(sjson,ops);
+		#endregion
 	}
 }
